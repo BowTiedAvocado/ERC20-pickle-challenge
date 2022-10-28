36e4e62ab3248d5fab5144f06bd58fbbea9674ff
BowTiedAvocado <bowtiedavocado@protonmail.com>
2022-10-28 12:49:10 -0500

    update

--- ERC20.sol
+++ ERC20.sol
@@ -3,8 +3,10 @@
 pragma solidity ^0.8.13;
 
 contract ERC20 {
-    string name;
-    string symbol;
+  string name;
+  string symbol;
+  uint256 maxSupply;
+  uint8 decimals;
 
-    constructor() {}
+  constructor() {}
 }
